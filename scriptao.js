c313485645d566a118d41977b39f28063c8cf533
Daniel R. Morais <42220133+danielrmorais@users.noreply.github.com>
2023-03-31 16:44:55 -0300

    PROJETO NÃO FINALIZADO
    Ainda falta a funcionalidade de cada tipo de text-transform

---
+++ scriptao.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('#form');
+const recipeInput = document.querySelector('#recipe');
+const recipeList = document.querySelector('#recipeList');
+
+let recipes = [];
+
+function addRecipe(e) {
+  e.preventDefault();
+  const recipe = recipeInput.value.trim();
+  if (!recipe) return;
+
+  recipes.push(recipe);
+
+  recipeInput.value = '';
+  recipeInput.focus();
+
+  render();
+}
+
+function deleteRecipe(index) {
+  recipes.splice(index, 1);
+  render();
+}
+
+function render() {
+  recipeList.innerHTML = '';
+  recipes.forEach((recipe, index) => {
+    const li = document.createElement('li');
+    li.innerText = recipe;
+    recipeList.appendChild(li);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = '🗑';
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.addEventListener('click', () => deleteRecipe(index));
+    li.appendChild(deleteBtn);
+  });
+}
+
+form.addEventListener('submit', addRecipe);
